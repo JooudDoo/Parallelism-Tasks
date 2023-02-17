098bba16370eda3e9d09bd995e7b2cc06760584b
joouddo <jj>
2023-02-17 13:57:18 +0000

    task1 summary + final code

--- task1/data.md
+++ task1/data.md
@@ -2,39 +2,77 @@
 
 ## float
 
+Programm 482824 us
+
 > result:  -0.028534
 
    Time(%)  | Time |    Calls  |   Avg  |       Min  |       Max  | Name 
 -----------|------|-----------|--------|------------|------------|------
-38.06% |  131.78us |        1 | 131.78us|  131.78us|  131.78us|  main_20_gpu  
-34.63% |  119.90us |        1 | 119.90us|  119.90us|  119.90us|  main_27_gpu  
-25.80% |  89.343us |        1 | 89.343us|  89.343us|  89.343us|  main_27_gpu__red  
+38.06% |  131.78us |        1 | 131.78us|  131.78us|  131.78us|  main_24_gpu  
+34.63% |  119.90us |        1 | 119.90us|  119.90us|  119.90us|  main_36_gpu  
+25.80% |  89.343us |        1 | 89.343us|  89.343us|  89.343us|  main_36_gpu__red  
 0.92%  | 3.2000us  |       1  |3.2000us | 3.2000us | 3.2000us | [CUDA memcpy DtoH]  
 0.59%  | 2.0480us  |       1  |2.0480us | 2.0480us | 2.0480us | [CUDA memcpy HtoD]  
 
 ## double
 
+Programm 479460 us 
+
 > result: -0.000000
 
    Time(%)  | Time |    Calls  |   Avg  |       Min  |       Max  | Name 
 -----------|------|-----------|--------|------------|------------|------
-35.85% |  131.71us |        1  | 131.71us  | 131.71us  | 131.71us |  main_27_gpu
-35.70% |  131.17us |        1  | 131.17us  | 131.17us  | 131.17us |  main_20_gpu
-27.17% |  99.839us |        1  | 99.839us  | 99.839us  | 99.839us |  main_27_gpu__red  
-0.78%  | 2.8480us  |       1   |2.8480us   |2.8480us   |2.8480us  | [CUDA memcpy DtoH]  
-0.51%  | 1.8560us  |       1   |1.8560us   |1.8560us   |1.8560us  | [CUDA memcpy HtoD]  
+35.85% |  131.71us |      1  | 131.71us  | 131.71us  | 131.71us |  main_36_gpu
+35.70% |  131.17us |      1  | 131.17us  | 131.17us  | 131.17us |  main_24_gpu
+27.17% |  99.839us |      1  | 99.839us  | 99.839us  | 99.839us |  main_36_gpu__red  
+0.78%  | 2.8480us  |      1   |2.8480us   |2.8480us   |2.8480us  | [CUDA memcpy DtoH]  
+0.51%  | 1.8560us  |      1   |1.8560us   |1.8560us   |1.8560us  | [CUDA memcpy HtoD]  
 
 
 # CPU
 
+## One core
+
 ## float
 
-> result: -0.027786
+Two cycles 47310 us  
+First cycle 43695 us  
+Second cycle 3615 us  
+Programm 47311 us  
+-0.043014  
 
-> result: 178548 us
+> result: -0.043014
 
 ## double
 
+Two cycles 67642 us  
+First cycle 60046 us  
+Second cycle 7595 us  
+Programm 67642 us  
+-0.000000  
+
 > 0.000000
 
-> 203043 us
+
+## Multi core
+
+## float
+
+Two cycles 22553 us  
+First cycle 17858 us  
+Second cycle 4695 us  
+Programm 22554 us  
+-0.043014  
+
+> result: -0.043014
+
+
+## double
+
+Two cycles 28765 us  
+First cycle 17811 us  
+Second cycle 10953 us  
+Programm 28766 us  
+-0.000000  
+
+> -0.000000
--- task1/task1.cpp
+++ task1/task1.cpp
@@ -4,14 +4,13 @@
 #include <iostream>
 #include <cmath>
 #include <chrono>
+#include <iomanip>
 
 #define PGI_ACC_TIME 1
 
 #define arraySize 10000000
 
-#define DDouble
-
-#ifdef DDouble
+#ifdef Double
 using floatingType = double;
 #else
 using floatingType = float;
@@ -23,28 +22,50 @@ int main(int argc, char const *argv[])
     floatingType sum = 0;
     floatingType* array = new floatingType[arraySize];
 
-    auto start = std::chrono::high_resolution_clock::now();
+    auto allProgramStart = std::chrono::high_resolution_clock::now();
 
     #pragma acc enter data create(array[0:arraySize], sum) copyin(pi)
     
+#ifdef CPU
+    auto allCycles = std::chrono::high_resolution_clock::now();
+    auto firstCycle = std::chrono::high_resolution_clock::now();
+#endif
+
     #pragma acc parallel loop present(array[0:arraySize],sum)
     for(int i = 0; i < arraySize; i++){
         array[i] = sin(2 * pi*i/arraySize);
     }
+
+#ifdef CPU
+    auto firstCycleElapsed = std::chrono::high_resolution_clock::now() - firstCycle;
+    auto secondCycle = std::chrono::high_resolution_clock::now();
+#endif
 
     #pragma acc parallel loop present(array[0:arraySize],sum) reduction(+:sum)
     for(int i = 0; i < arraySize; i++){
         sum += array[i];
     }
 
+#ifdef CPU
+    auto allCyclesElapsed = std::chrono::high_resolution_clock::now() - allCycles;
+    auto secondCycleElapsed = std::chrono::high_resolution_clock::now() - secondCycle;
+#endif
+
     #pragma acc exit data copyout(sum) delete(array[0:arraySize], pi)
 
-    auto elapsed = std::chrono::high_resolution_clock::now() - start;
-    long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(
-        elapsed).count();
+    auto allProgramElapsed = std::chrono::high_resolution_clock::now() - allProgramStart;
+    long long programMicro = std::chrono::duration_cast<std::chrono::microseconds>(allProgramElapsed).count();
     std::cout << std::fixed;
-    std::cout << microseconds << std::endl;
-    std::cout << sum << std::endl;
+#ifdef CPU
+    long long allCyclesMicro = std::chrono::duration_cast<std::chrono::microseconds>(allCyclesElapsed).count();
+    long long firstCyclesMicro = std::chrono::duration_cast<std::chrono::microseconds>(firstCycleElapsed).count();
+    long long secondCyclesMicro = std::chrono::duration_cast<std::chrono::microseconds>(secondCycleElapsed).count();
+    std::cout << "Two cycles " << allCyclesMicro << " us" << std::endl;
+    std::cout << "First cycle " << firstCyclesMicro << " us" << std::endl;
+    std::cout << "Second cycle " << secondCyclesMicro << " us" << std::endl;
+#endif
+    std::cout << "Programm " << programMicro << " us" << std::endl;
+    std::cout << std::setprecision(20) << sum << std::endl;
 
     delete[] array;
 
