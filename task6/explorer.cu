a19e3b30a714eb32ab152482d9bf40fa04558bbe
joouddo <>
2023-05-14 23:51:08 +0700

    nvtxtrace

--- task6/explorer.cu
+++ task6/explorer.cu
@@ -2,6 +2,9 @@
 
 #include <iostream>
 
+#ifdef NVPROF_
+#include <nvToolsExt.h>
+#endif
 
 int main(){
     
@@ -14,13 +17,21 @@ int main(){
     Net.add_layer(create_layer<FC>(1024, 256, "torch/fc1_weights", "torch/fc1_bias"));
     Net.add_layer(create_layer<Sigmoid>());
 
+
     Net.add_layer(create_layer<FC>(256, 16, "torch/fc2_weights", "torch/fc2_bias"));
     Net.add_layer(create_layer<Sigmoid>());
 
     Net.add_layer(create_layer<FC>(16, 1, "torch/fc3_weights", "torch/fc3_bias"));
     Net.add_layer(create_layer<Sigmoid>());
 
+
+#ifdef NVPROF_
+    nvtxRangePush("Forward_pass");
+#endif
     MDT_array<calc_type> b = Net.forward(a);
+#ifdef NVPROF_
+    nvtxRangePop();
+#endif
 
     std::cout << b(0) << std::endl;
 
